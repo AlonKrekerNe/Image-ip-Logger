851a8384b245751584d723dd05230c0d3c294aca
AlonKrekerNe <80860002+AlonKrekerNe@users.noreply.github.com>
2021-03-22 11:57:34 +0100

    Create Logger.php

---
+++ Logger.php
@@ -0,0 +1,24 @@
+<?php
+$log = 'logger.html';
+$ip = $_SERVER['REMOTE_ADDR'];
+$page = $_SERVER['REQUEST_URI'];
+$refer = $_SERVER['HTTP_REFERER'];
+$date_time = date("l j F Y  g:ia", time() - date("Z")) ;
+$agent = $_SERVER['HTTP_USER_AGENT'];
+$fp = fopen("logger.php", "a");
+fputs($fp, "
+<b>$date_time</b> <br> <b>IP: </b>$ip<br><b>Page: </b>$page<br><b>Refer: </b>$refer<br><b>Useragent:
+
+</b>$agent <br><br>
+");
+flock($fp, 3);
+fclose($fp);
+
+header( "Content-type: image/png" );
+imagepng( $my_img );
+imagecolordeallocate( $line_color );
+imagecolordeallocate( $text_color );
+imagecolordeallocate( $background );
+imagedestroy( $my_img );
+
+?>
